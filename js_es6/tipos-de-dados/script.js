568408dab4701b7ab87345688a0fe08e0d926036
Cassiano Thurow <cassiano.thurow@gmail.com>
2020-07-31 15:42:35 -0300

    .

--- js_es6/tipos-de-dados/script.js
+++ js_es6/tipos-de-dados/script.js
@@ -1,12 +1,36 @@
-let nome = "Cassiano"; //string
-let idade = 25; //number
-let possuiFaculdade = true; // boolean
-let time; // undefined
-let comida = null; //null
-let simbolo = Symbol(); //Symbol
-let novoObjeto = {}; //object
+// let nome = "Cassiano"; //string
+// let idade = 25; //number
+// let possuiFaculdade = true; // boolean
+// let time; // undefined
+// let comida = null; //null
+// let simbolo = Symbol(); //Symbol
+// let novoObjeto = {}; //object
 
-// Verificar tipo de variavel;
+// Verificar tipo de variável;
 
+// let sobrenome = "thurow";
+// console.log(typeof sobrenome); //string
+
+//--------
+
+// let nome = "cassiano";
+
+// let nomeCompleto = `${nome} ${sobrenome}`;
+// console.log(nomeCompleto);
+
+// let gols = 50;
+// let frase = `Eu fiz ${gols} gols`;
+
+// testeAspas = "isso é uma frase com 'aspas'";
+
+//exercício:
+
+let frase1 = "Frase";
+let frase2 = "Numero 2";
+let idade = 25;
+let nome = "cassiano";
 let sobrenome = "thurow";
-console.log(typeof sobrenome); //string
+let nomeCompleto = `${nome} ${sobrenome}`;
+
+frase3 = `it's time`;
+console.log(typeof nome);
