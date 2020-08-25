b71164dacb85e355e94548ffb179224901565f5c
Cassiano Thurow <cassiano.thurow@gmail.com>
2020-08-25 15:43:08 -0300

    Boolean

---
+++ js_es6/booleans/script.js
@@ -0,0 +1,28 @@
+var possuiFaculdade = true;
+
+if (possuiFaculdade) {
+  console.log("Possui faculdade");
+} else {
+  console.log("Não possui faculdade");
+}
+
+var teste = true;
+
+if (teste) {
+  x = 10;
+} else {
+  x = 5;
+}
+
+console.log(x);
+
+var possuiGraduacao = true;
+var possuiDoutorado = false;
+
+if (possuiDoutorado) {
+  console.log("Possui Graduacao e doutorado");
+} else if (possuiGraduacao) {
+  console.log("possui graduacao, mas nao possui doutorado");
+} else {
+  console.log("Nao possui graduacao");
+}
