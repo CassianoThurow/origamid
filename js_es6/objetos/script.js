2b8a67c0aef1c34d7d3d3eb6c7b152ff6d2561c8
Cassiano Thurow <cassiano.thurow@gmail.com>
2020-08-28 14:11:04 -0300

    Objects

---
+++ js_es6/objetos/script.js
@@ -0,0 +1,24 @@
+var pessoa = {
+    nome: 'Cassiano',
+    idade: 25,
+    profissao: 'FrontEnd',
+    possuiFaculdade = true
+}
+
+pessoa.nome; //cassiano
+
+var quadrado = {
+    lados: 4,
+    area(lado){
+        return lado * lado;
+    },
+    perimento(lado) {
+        return this.lados * lado;
+ }   
+}
+quadrado.lados //4
+quadrado.area(5) //25
+quadrado.perimento(5)//20
+
+
+Math.PI()
