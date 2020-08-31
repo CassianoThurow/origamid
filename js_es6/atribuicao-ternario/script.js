dced9f51d52d6c7fcee809ee5f8e95ffa7edc1a2
Cassiano Thurow <cassiano.thurow@gmail.com>
2020-08-31 17:42:39 -0300

    Ternario/Escopo

---
+++ js_es6/atribuicao-ternario/script.js
@@ -0,0 +1,11 @@
+var numero = 20;
+
+numero -= 20;
+console.log(numero);
+
+var idade = 19;
+
+// Condicional ternário
+var podeBeber = idade >= 18 ? "pode beber" : "nao pode beber";
+
+console.log(podeBeber);
