c28bb4b3a275e8b2ce535f46d40d4b56b263d386
Cassiano Thurow <cassiano.thurow@gmail.com>
2020-09-01 16:10:51 -0300

    dom

---
+++ dom/iniciante/exercicios.js
@@ -0,0 +1,16 @@
+// Retorne o url da página atual utilizando o objeto window
+
+const hrefUrl = window.location.href;
+
+// Seleciona o primeiro elemento da página que
+// possua a classe ativo
+
+const activeElement = querySelector(".ativo");
+
+// Retorne a linguagem do navegador
+
+const browserLanguage = navigator.language;
+
+// Retorne a largura da janela
+
+const widthWindow = window.innerWidth;
---
+++ dom/iniciante/script.js
@@ -0,0 +1,11 @@
+// window.alert("Alerta");
+
+const href = window.location.href;
+
+console.log(href);
+
+if (href === "http://localhost:5500/dom/iniciante/")
+  console.log("é a mesma pagina");
+else console.log(" é diferente");
+
+const h1Select = document.querySelector("h1");
