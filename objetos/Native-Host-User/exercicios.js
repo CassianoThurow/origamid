9377290b33fbfd80a21d82bb9754c7f626134b8a
Cassiano Thurow <cassiano.thurow@gmail.com>
2020-11-10 14:21:04 -0300

    native, host user

---
+++ objetos/Native-Host-User/exercicios.js
@@ -0,0 +1,29 @@
+// Liste 5 objetos nativos
+Object
+Array
+String
+Number
+Function
+
+
+// Liste 5 objetos do browser
+NodeList
+HTMLCollection
+HTMLLIElement
+HTMLVideoElement
+NodeFilter
+Window
+Document
+
+// Liste 2 Métodos, Propriedades ou Objetos
+// presentes no Chrome que não existem no Firefox
+if( typeof document.webkitHidden !== "undefined"){
+    console.log("existe")
+}else{
+    console.log("não existe")
+}
+if( typeof document.webkitVisibilityState !== "undefined"){
+    console.log("existe")
+}else{
+    console.log("não existe")
+}
---
+++ objetos/Native-Host-User/script.js
@@ -0,0 +1,13 @@
+console.log(typeof Array.from)
+console.log(typeof Array.ssd !== "undefined") 
+
+
+
+
+/*
+API
+Application Programming Interface, é uma interface de software criada para a interação com outros softwares. Ou seja, toda interação que fazemos com o browser utilizando Objetos, Métodos e Propriedades, estamos na verdade interagindo com a API do browser.
+*/
+
+
+
