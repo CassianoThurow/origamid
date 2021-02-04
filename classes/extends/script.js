700603e4d716e8df9f26c4728207b45f8ecb6a0e
Cassiano Thurow <cassiano.thurow@gmail.com>
2021-02-04 16:02:57 -0300

    ✨ extends

--- classes/extends/script.js
+++ classes/extends/script.js
@@ -16,3 +16,62 @@ class Veiculo {
   const honda = new Moto(2);
   honda.empinar();
   
+
+
+
+  class Veiculo {
+    constructor(rodas) {
+      this.rodas = rodas;
+    }
+    acelerar() {
+      console.log('Acelerou');
+    }
+  }
+  
+  class Moto extends Veiculo {
+    acelerar() {
+      console.log('Acelerou muito');
+    }
+  }
+  
+  const honda = new Moto(2);
+  honda.acelerar();
+  
+
+
+  class Veiculo {
+    constructor(rodas) {
+      this.rodas = rodas;
+    }
+    acelerar() {
+      console.log('Acelerou');
+    }
+  }
+  
+  class Moto extends Veiculo {
+    acelerar() {
+      super.acelerar();
+      console.log('Muito');
+    }
+  }
+  
+  const honda = new Moto(2);
+  honda.acelerar();
+  
+
+  class Veiculo {
+    constructor(rodas, combustivel) {
+      this.rodas = rodas;
+      this.combustivel = combustivel;
+    }
+  }
+  
+  class Moto extends Veiculo {
+    constructor(rodas, combustivel, capacete) {
+      super(rodas, combustivel);
+      this.capacete = capacete;
+    }
+  }
+  
+  const honda = new Moto(4, 'Gasolina', true);
+  
